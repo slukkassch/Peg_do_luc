0cc2e591f842a65978a6f1cb2b75a37723d28621
Luccas schmidt <150750093+slukkassch@users.noreply.github.com>
2024-11-24 17:50:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Tecnologia',
+    'O que é uma Alexa?',
+    'Alexa é uma assistente virtual desenvolvida pela Amazon'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Coréia do Sul?',
+    'A capital da Coréia do Sul é CU'
+)
+
+criaCartao(
+    'Design',
+    'O que é o Figma?',
+    'Figma é um aplicativo de desenvolvimento de protótipo'
+)
+
+criaCartao(
+    'Religião',
+    'O que é uma Cerimoniário na igreja católica?',
+    'Um cerimoniário da igreja católica é uma pessoa que está sem a dispossição para ajudar o padre na santa Missa'
+)
+
+criaCartao(
+    'Religião',
+    'O que é uma cadeia da consagração?',
+    'uma cadeia da consagração é uma pulseira que contém um cadeado e uma medalha do santo da escolha do usuário'
+)
+
+criaCartao(
+    'Religião',
+    'Quando termina o ano da igreja católica?',
+    'O ano da igreja católica termina no dia de Cisto Rei'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Brasil?',
+    'Brasília'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quantos dias tem uma semana?',
+    'Sete'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual é o maior planeta do sistema solar?',
+    'Júpiter'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o estado físico da água a 0°C?',
+    'Sólido'
+)
